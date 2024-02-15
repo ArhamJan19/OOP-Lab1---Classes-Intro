f03fd8f4a0f0a69c5dad5425fb88b7608113bd99
ArhamJan19 <146324130+ArhamJan19@users.noreply.github.com>
2024-02-15 21:59:40 +0500

    Add project files.

---
+++ Task.cpp
@@ -0,0 +1,44 @@
+
+
+#include<iostream>
+using namespace std;
+
+class House {
+public:
+
+};
+
+class Dog {
+public:
+	string name;
+	string Breed;
+	int age;
+
+	void bark() {
+		cout << name << " says Woof!" << endl;
+	}
+};
+
+class Person {
+public:
+	string name;
+	Dog pet;
+	void DisplayDogInfo()
+	{
+		cout << "My name is " << name << " my dogs name is " << pet.name << " he is a " << pet.Breed << " and is " << pet.age << " years old." << endl;
+	}
+	void introducePet()
+	{
+		cout << "My dog's name is " << pet.name << endl; pet.bark();
+	}
+};
+
+int main() {
+	Person person1;
+	person1.name = "Alice";
+	person1.pet.name = "Buddy";
+	person1.pet.age = 15;
+	person1.pet.Breed = "German Shephard";
+	person1.DisplayDogInfo();
+	return 0;
+}
